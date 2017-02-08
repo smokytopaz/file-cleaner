f0b8af5f1a90eee4d79ff56fd347016ed51c6c18
smokytopaz <smokytopaz42@gmail.com>
2017-02-08 10:17:18 -0700

    Update fileCleaner.sh

--- fileCleaner.sh
+++ fileCleaner.sh
@@ -6,8 +6,8 @@ fileCount1=`ls /path/to/file/filename* | wc -l`
 fileCount2=`ls /path/to/file/filename* | wc -l`
 
 if [ $fileCount1 -gt 20 ] && [ $fileCount2 -gt 20 ]; then
-    /usr/bin/find /path/to/directory -type f -name '*.gz' -mtime +20 -exec rm {} \;
-    /usr/bin/find /path/to/directory -type f -name '*.gz' -mtime +20 -exec rm {} \;
+    /usr/bin/find /path/to/directory1 -type f -name '*.gz' -mtime +20 -exec rm {} \;
+    /usr/bin/find /path/to/directory2 -type f -name '*.gz' -mtime +20 -exec rm {} \;
 else
     exit 0
 fi
