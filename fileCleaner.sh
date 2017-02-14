616cffa615f6b6421e6593ec038be4b89b30f9f0
smokytopaz <smokytopaz42@gmail.com>
2017-02-14 14:08:55 -0700

    cleanup.formatting

--- fileCleaner.sh
+++ fileCleaner.sh
@@ -2,9 +2,9 @@
 
 #Usage - fileCleaner.sh /path/to/file filename daysToKeep(Integer)
 
-# This script uses the above arguments to delete matching files that are 
-# older than the amount of days you want to keep. It performs a check to 
-# verify that it has at least the amount of files as days specified before 
+# This script uses the above arguments to delete matching files that are
+# older than the amount of days you want to keep. It performs a check to
+# verify that it has at least the amount of files as days specified before
 # deleting.
 
 fileCount=`ls $1/$2 | wc -l`
